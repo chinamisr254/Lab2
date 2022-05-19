76d12b306d71ca7a05604e4da343715b427a5f12
Joseph Hawkins <JosephHawkins1776@gmail.com>
2022-05-19 13:41:22 -0400

    Initial Commit

---
+++ Lab2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab2
+//
+//  Created by Joseph Hawkins on 5/19/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
